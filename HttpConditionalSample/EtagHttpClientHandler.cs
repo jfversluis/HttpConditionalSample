52aa8c9751f6e350eb2270e4cc517eb01c62623d
Gerald Versluis <gerald@verslu.is>
2018-05-29 10:15:35 +0200

    Initial commit

---
+++ HttpConditionalSample/EtagHttpClientHandler.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HttpConditionalSample
+{
+	public class EtagHttpClientHandler : HttpClientHandler
+	{
+		private readonly Dictionary<string, string> _etagDictionary = new Dictionary<string, string>();
+
+		protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+		{
+			// See if we have a ETag for this endpoint and add it to the request
+			_etagDictionary.TryGetValue(request.RequestUri.ToString(), out var etag);
+
+			if (!string.IsNullOrWhiteSpace(etag))
+			{
+				request.Headers.IfNoneMatch.Clear();
+				request.Headers.IfNoneMatch.Add(new EntityTagHeaderValue(etag));
+			}
+
+			// Everything before the base call is the request
+			var responseMessage = await base.SendAsync(request, cancellationToken).ConfigureAwait(false);
+
+			// Here we can handle the response
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				// If a ETag is provided with the response, cache it for future requests
+				if (!string.IsNullOrWhiteSpace(responseMessage.Headers.ETag?.Tag))
+					_etagDictionary.Add(responseMessage.RequestMessage.RequestUri.ToString(), responseMessage.Headers.ETag?.Tag);
+			}
+
+			return responseMessage;
+		}
+	}
+}
---
+++ HttpConditionalSample/MainPage.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Xamarin.Forms;
+
+namespace HttpConditionalSample
+{
+	public partial class MainPage : ContentPage, INotifyPropertyChanged
+	{
+		private readonly HttpClient _customHttpClient;
+		private readonly EtagHttpClientHandler _customHttpClientHandler;
+
+		private const string ApiBaseUrl = "http://airplanemodeproof-api.azurewebsites.net/api/";
+		bool isLoading;
+
+		public bool IsLoading
+		{
+			get
+			{
+				return isLoading;
+			}
+
+			set
+			{
+				isLoading = value;
+				OnPropertyChanged(nameof(IsLoading));
+			}
+		}
+
+		public ObservableCollection<Superhero> Superheroes { get; } = new ObservableCollection<Superhero>();
+		public Command RefreshDataCommand { get; }
+
+		public MainPage()
+		{
+			InitializeComponent();
+
+			_customHttpClientHandler = new EtagHttpClientHandler();
+			_customHttpClient = new HttpClient(_customHttpClientHandler)
+			{
+				BaseAddress = new Uri(ApiBaseUrl)
+			};
+
+			RefreshDataCommand = new Command(async () => await GetSuperheroes());
+
+
+			BindingContext = this;
+
+			RefreshDataCommand.Execute(null);
+		}
+
+		public async Task GetSuperheroes()
+		{
+			IsLoading = true;
+
+			try
+			{
+				var superheroesJson = await _customHttpClient.GetStringAsync("superhero");
+				var superheroes = JsonConvert.DeserializeObject<Superhero[]>(superheroesJson);
+
+				Superheroes.Clear();
+
+				foreach (var hero in superheroes)
+					Superheroes.Add(hero);
+			}
+			catch (HttpRequestException ex) when (ex.Message.Contains("304"))
+			{
+				// Intentionally left blank, 304 response is fine
+			}
+			finally
+			{
+				IsLoading = false;
+			}
+		}
+	}
+}
